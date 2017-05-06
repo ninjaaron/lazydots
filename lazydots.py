8c0eaa8b6dd5932430a81119c420ae0d5e77eff2
Aaron Christianson <ninjaaron@gmail.com>
2017-05-06 21:25:22 +0200

    adding the web interface

--- lazydots.py
+++ lazydots.py
@@ -1,7 +1,12 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 import unicodedata
+import json
 from pathlib import Path
 import functools
 import deromanize
+from deromanize import trees
 import yaml
 
 CONFIG_FILE = Path(__file__).parent/'data.yml'
@@ -60,6 +65,24 @@ def make_pointy_text(text):
     return '\n'.join(map(make_pointy_line, text.splitlines()))
 
 
+def clean_key(key, tree=trees.Trie):
+    return tree({k: v[0][1] for k, v in key.simplify().items()}).serializable()
+
+
+def special_dump():
+    data = {k: json.dumps(clean_key(keys[k]), ensure_ascii=False)
+            for k in ('front', 'mid')}
+    data['end'] = json.dumps(
+        clean_key(keys['end'], trees.BackTrie), ensure_ascii=False)
+    return '\n'.join((
+        'data = {{',
+        '    front: {front},',
+        '    mid: {mid},',
+        '    end: {end},',
+        '}}'
+    )).format(**data)
+
+
 def read_text():
     import sys
     import argparse
@@ -70,7 +93,13 @@ def read_text():
         help='ascii text to make pointy')
     add('-n', '--normalize', action='store_true',
         help='apply canonical normalization')
+    add('-d', '--dump', action='store_true',
+        help='make a very special dump')
     args = ap.parse_args()
+
+    if args.dump:
+        print(special_dump())
+        sys.exit(0)
 
     pointy = '\n'.join(make_pointy_line(l) for l in args.string)
     if args.normalize:
---
+++ web/logic.js
@@ -0,0 +1,116 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+theinput = []
+function getevent(e)
+{
+    var words = e.target.value.split(/(\s)/)
+    words = words.map(function(word) {
+	if (/\s/.test(word)) {
+	    return word
+	} else {
+	    try {
+		return make_pointy(word)
+	    } catch (e) {
+		return word
+	    }
+	}
+    })
+    // var output = trieop.getallparts(data.mid, e.target.value)
+    poo.value = words.join('')
+}
+
+
+Trie = function(root)
+{
+    this.root = root
+}
+Trie.prototype.getpart = function(str)
+{
+    var node = this.root
+    var value = null
+    var remainder = str
+    var i
+    var chr
+    for (i=0; i < str.length; ++i) {
+	chr = str[i]
+	node = node[1][chr]
+	if (node === undefined) {
+	    if (value === null)
+		throw 'KeyError: ' + str
+	    else
+		return {value: value, remainder: remainder}
+	}
+	if (node[0] !== null) {
+	    value = node[0]
+	    remainder = str.slice(i+1)
+	}
+    }
+    if (value === null)
+	throw 'KeyError' + str
+    else
+	return {value: value, remainder: remainder}
+}
+Trie.prototype.getallparts = function(str)
+{
+    var results = []
+    var vr = {remainder: str}
+    while (vr.remainder) {
+	vr = this.getpart(vr.remainder)
+	results.push(vr.value)
+    }
+    return results
+}
+
+data.front = new Trie(data.front)
+data.mid = new Trie(data.mid)
+data.end = new Trie(data.end)
+
+function get_end(word)
+{
+    word = word.split('').reverse().join('')
+    var end = data.end.getpart(word)
+    end.remainder = end.remainder.split('').reverse().join('')
+    return end
+}
+
+function make_pointy(word)
+{
+    var end
+    var front
+    var middle
+    end = get_end(word)
+    if (end.remainder !== '') {
+	try {
+	    front = data.front.getpart(end.remainder)
+	} catch (e) {
+	    return no_end(word)
+	}
+    } else {
+	return no_end(word)
+    }
+    if (front.remainder !== '') {
+	middle = data.mid.getallparts(front.remainder).join('')
+	return front.value + middle + end.value
+    } else {
+	return front.value + end.value
+    }
+}
+function no_end(word)
+{
+    var front
+    var end
+    var middle
+    front = data.front.getpart(word)
+    if (front.remainder !== '') {
+	end = get_end(front.remainder)
+	if (end.remainder !== '') {
+	    middle = data.mid.getallparts(end.remainder).join('')
+	    return front.value + middle + end.value
+	} else {
+	    return front.value + end.value
+	}
+    } else {
+	return front.value
+    }
+}
